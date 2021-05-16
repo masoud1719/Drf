c11de28401d862acc92b2e2b493144b20eb4b6d8
Masoud <masoud.a.1719@gmail.com>
2021-05-16 11:14:09 -0700

    adding revoke token route by using APIView class view

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('users', views.UserListView.as_view()),
     path('users/create', views.UserCreateView.as_view()),
     path('users/<int:pk>', views.UserDetailView.as_view()),
+    path('users/revoke_token', views.RevokeToken.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView, \
     ListAPIView
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from rest_framework.views import status
 from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
 from django.contrib.auth.models import User
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from .permissions import IsSuperUser, IsStaffOrReadOnly, IsOwnerOrReadOnly, IsSuperorAnanymouswriteonly
 
 
@@ -26,15 +29,17 @@ class UserListView(ListAPIView):
     def get_queryset(self):
         print(self.request.user)
         return User.objects.all()
+
     serializer_class = UserSerializer
 
 
 class UserCreateView(CreateAPIView):
-
     permission_classes = [AllowAny]
+
     def get_queryset(self):
         print(self.request.user)
         return User.objects.all()
+
     serializer_class = UserSerializer
 
 
@@ -44,4 +49,14 @@ class UserDetailView(RetrieveAPIView):
     def get_queryset(self):
         print(self.request.user)
         return User.objects.all()
+
     serializer_class = UserSerializer
+
+
+class RevokeToken(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+        request.auth.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
